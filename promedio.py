bb591b3534b35fc5660c442b882d3b69710c6feb
edwardrg97 <edwardrg97@gmail.com>
2019-08-20 00:14:45 -0500

    Primer_codigo

---
+++ promedio.py
@@ -0,0 +1,11 @@
+promedio, total, contar = 0.0, 0, 0
+
+print ("Introduzca la nota de un estudiante (-1 para salir): ")
+grado = int(input())	
+while grado != -1:
+    total = total + grado
+    contar = contar + 1
+    print ("Introduzca la nota de un estudiante (-1 para salir): ")
+    grado = int(input())
+promedio = total / contar
+print ("Promedio de notas del grado escolar es: " + str(promedio))
